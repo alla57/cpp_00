1f0d14c1e5ee275ecf78bcc375277ace2004d0fb
Alla eddine BOUMEDIENE <alboumed@e1r3p5.clusters.42paris.fr>
2022-08-24 17:12:23 +0200

    cpp04 ex01 main set like told in subject

--- cpp_04/ex01/main.cpp
+++ cpp_04/ex01/main.cpp
@@ -5,25 +5,45 @@
 
 int main()
 {
-	// const Animal* meta = new Animal();
-	Dog *chien = new Dog();
-	Dog *chien2 = new Dog();
-	std::cout << chien->getBrain()->getIdea(0) << std::endl;
-	chien->setBrainIdea("bonjour", 0);
-	std::cout << chien->getBrain()->getIdea(0) << std::endl;
-	std::cout << chien2->getBrain()->getIdea(0) << std::endl;
+	{
+		Animal *animalTab[2];
+		int i = -1;
+		while (++i < 1)
+		{
+			animalTab[i] = new Dog;
+		}
+		while (i < 2)
+		{
+			animalTab[i++] = new Cat;
+		}
+		i = 0;
+		while (i < 2)
+			delete animalTab[i++];
+		std::cout << std::endl;
+	}
+	{
+		Dog *chien = new Dog();
+		Dog *chien2 = new Dog();
 
-	Cat *chat = new Cat();
-	Cat *chat2 = new Cat();
-	std::cout << chat->getBrain()->getIdea(0) << std::endl;
-	chat->setBrainIdea("au revoir", 0);
-	std::cout << chat->getBrain()->getIdea(0) << std::endl;	
-	std::cout << chat2->getBrain()->getIdea(0) << std::endl;
+		std::cout << chien->getBrain()->getIdea(0) << std::endl;
 
-	// delete meta;
-	delete chien;
-	delete chien2;
-	delete chat;
-	delete chat2;
-	return (0);
+		chien->setBrainIdea("bonjour", 0);
+		std::cout << chien->getBrain()->getIdea(0) << std::endl;
+
+		std::cout << chien2->getBrain()->getIdea(0) << std::endl;
+
+		Cat *chat = new Cat();
+		Cat *chat2 = new Cat();
+
+		std::cout << chat->getBrain()->getIdea(0) << std::endl;
+		chat->setBrainIdea("au revoir", 0);
+		std::cout << chat->getBrain()->getIdea(0) << std::endl;	
+		std::cout << chat2->getBrain()->getIdea(0) << std::endl;
+
+		delete chien;
+		delete chien2;
+		delete chat;
+		delete chat2;
+		return (0);
+	}
 }
